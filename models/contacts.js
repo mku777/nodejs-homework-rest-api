e50055a77a9fa723786a2abd64bb21e84035b419
mku777 <mkm.kuz777@gmail.com>
2023-01-20 18:56:52 +0200

    logic+npm

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,82 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "./contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const [contact] = contacts.filter(
+      (contact) => contact.id === String(contactId)
+    );
+    if (!contact) {
+      throw new Error(404, "Contact not found");
+    }
+    return contact;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (id) => {
+  try {
+    const contacts = await listContacts();
 
-const updateContact = async (contactId, body) => {}
+    const contactIndex = contacts.findIndex(
+      (contact) => contact.id === String(id)
+    );
+    if (contactIndex === -1) {
+      throw new Error("Contact not found");
+    }
+
+    const [removedContact] = contacts.splice(contactIndex, 1);
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return removedContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const addContact = async (body) => {
+  try {
+    const contacts = await listContacts();
+    contacts.push(body);
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return body;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const updateContact = async (id, body) => {
+  const { name, email, phone } = body;
+  try {
+    const contacts = await listContacts();
+
+    const contactIndex = contacts.findIndex(
+      (contact) => contact.id === String(id)
+    );
+    if (contactIndex === -1) {
+      throw new Error("Contact not found");
+    }
+
+    contacts[contactIndex] = { id, name, email, phone };
+
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return contacts[contactIndex];
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +84,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
